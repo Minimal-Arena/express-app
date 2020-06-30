534c36e9095633571624042bb096554df9368e54
Jeffrey Orndorff <jeffrey.orndorff@gmail.com>
2020-06-30 14:04:08 -0700

    Add email field to register

--- data/seeds/01-users.js
+++ data/seeds/01-users.js
@@ -1,6 +1,6 @@
 exports.seed = function (knex) {
     return knex('users').insert([
-        { username: "Test User", password: "$2b$11$AdsDMOOnNWJH/GV0I6EVk.KROYPClTOgTMHjR31oXUblUS1UxiUDm" },
-        { username: "Some User", password: "$2b$11$ccVujqV0PC74/Vs5ch78XumD6l3LFnHzhIFXuvo6Dk5umnw0BuScW" }
+        { username: "Test User", email:"test@test.com", password: "$2b$11$AdsDMOOnNWJH/GV0I6EVk.KROYPClTOgTMHjR31oXUblUS1UxiUDm" },
+        { username: "Some User", email: "user@user.com", password: "$2b$11$ccVujqV0PC74/Vs5ch78XumD6l3LFnHzhIFXuvo6Dk5umnw0BuScW" }
     ])
 }
